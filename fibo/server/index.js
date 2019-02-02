77fd6992b09c4eaf964f7443eb4d75956d0a5398
Arjun Ramamurthy <arjun.r.m@gmail.com>
2019-02-02 18:28:23 +0530

    kubernetes - added conf file for pod, service and deployment for multi-fibo

--- fibo/server/index.js
+++ fibo/server/index.js
@@ -31,4 +31,51 @@ pgClient
     .query('CREATE TABLE IF NOT EXISTS values (number INT)')
     .catch(err => console.log(err));
 
+// redis client setup
+const redis = require('redis')
+const redisClient = redis.createClient({
+    host: keys.redisHost,
+    port: keys.redisPort,
+    retry_strategy: () => 1000
+});
+const redisPublisher = redisClient.duplicate();
 
+// express route handlers
+app.get('/', (req, resp) => {
+    res.send('Hi');
+});
+
+
+app.get('/values/all', async (req, resp) => {
+    const values = await pgClient.query('SELECT * from values');
+
+    res.send(values.rows);
+});
+
+app.get('/values/current', async (req, res) => {
+    redisClient.hgetall('values', (err, values) => {
+        res.send(values);
+    });
+});
+
+app.post('/values', async (req, res) => {
+    const index = req.body.index;
+
+    // to avoid worker thread getting blocked limit max index
+    if (parseInt(index) > 40) {
+        return res.status(422).send('Index too high');
+    }
+
+    // store index, 'Nothing yet!' as fibo is not yet calculated
+    redisClient.hset('values', index, 'Nothing yet!');
+    // publish to listeners that a value has been inserted into redis
+    redisPublisher.publish('insert', index);
+    // store the index into postgres
+    pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
+
+    res.send({working: true});
+});
+
+app.listen(5000, err => {
+    console.log('Listening');
+});
